cad5c516546dfc53fc9d525cbc4ff990f9926475
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-03-13 18:25:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <cstdlib>
 
 using namespace std;
 
+	
+
 int main(){
+	ifstream infile;
+	ofstream outfile;
+	
+	int cCount=0;
+	int A=0;
+	int F=0;
+	int a=0;
+	int b=0;
+	
+	infile.open("info.dat");
+	
+	infile.open("junk.txt");
+	if(infile.fail()){
+		cout << "Failed!\n";
+		exit(1);
+	}
+	
+	while(!infile.eof()){
+		
+	
+	
 	
 	
 	
